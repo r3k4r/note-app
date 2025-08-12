cef7c1893c7be9f8a2309a32061bb81d5fba36c8
r3k4r <rekarjamal33@gmail.com>
2025-08-12 19:05:55 +0300

    Update page.jsx

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -62,24 +62,24 @@ const Login = () => {
   }
 
   return (
-    <div className='max-w-6xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-10 flex items-center justify-between'>
+    <div className='max-w-6xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-6 md:py-10 flex flex-col md:flex-row items-center justify-center md:justify-between gap-8 md:gap-4'>
         {/* LEFT-SIDE */}
-        <div className='w-fit'>
+        <div className='w-full md:w-fit flex flex-col items-center'>
             {/* SVG */}
-            <Image src={'/Frame.svg'} alt='' width={360} height={360} />
+            <Image src={'/Frame.svg'} alt='' width={360} height={360} className="w-[240px] h-[240px]  md:w-[360px] md:h-[360px]" />
 
             <div className=''>
-                <h1 className="text-5xl py-2 font-bold text-center mt-2 bg-gradient-to-r from-left to-right text-transparent bg-clip-text">
+                <h1 className="text-3xl md:text-4xl py-2 font-bold text-center mt-2 bg-gradient-to-r from-left to-right text-transparent bg-clip-text">
                     Organize <br /> yourself here!
                 </h1>
-                <p className="text-[#A89085] mt-4 text-2xl text-center font-medium">Practical, fast and free!</p>
+                <p className="text-[#A89085] mt-2 md:mt-4 text-xl  text-center font-medium">Practical, fast and free!</p>
             </div>
         </div>
 
         {/* Login form || RIGHT-SIDE*/}
-        <div className="w-full max-w-md p-8 rounded-lg">
-            <form onSubmit={handleSubmit} className="space-y-6">
-                <div className="space-y-2">
+        <div className="w-full max-w-md p-4 sm:p-6 md:p-8 rounded-lg">
+            <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6">
+                <div className="space-y-1 md:space-y-2">
                 <label htmlFor="email" className="text-sm font-medium text-gray-700 uppercase tracking-wide">
                     EMAIL
                 </label>
@@ -95,7 +95,7 @@ const Login = () => {
                 />
                 </div>
 
-                <div className="space-y-2">
+                <div className="space-y-1 md:space-y-2">
                 <label htmlFor="password" className="text-sm font-medium text-gray-700 uppercase tracking-wide">
                     PASSWORD
                 </label>
@@ -111,7 +111,7 @@ const Login = () => {
                 />
                 </div>
 
-                <div className='flex justify-end'>
+                <div className='flex justify-end mt-4'>
                     <button
                       type="submit"
                       className="w-fit px-6 py-1 bg-gradient-to-r from-left to-right text-white font-medium rounded-md disabled:opacity-70"
