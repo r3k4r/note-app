b01087be4f20eeec43d9afe217fcbbfe47dfcb5a
r3k4r <rekarjamal33@gmail.com>
2025-08-13 10:26:50 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ import ThemeProvider from '@/components/theme/ThemeProvider'
 export const metadata = {
   title: 'Note App',
   description:
-    'This Notes App is a modern, responsive web application designed to help users efficiently manage their notes with a priority-based organization system. Built with React (Next.js) and Tailwind CSS, the app features a clean, intuitive interface that supports creating, editing, and deleting notes, each categorized into three priority levels — Urgent, High, and Low. Users can register, log in, and manage their notes in real time, with changes reflected instantly without page reloads. The application is integrated with a MockAPI backend for persistent storage and offers additional features like grid/list view switching, responsive layouts for mobile and desktop, and smooth animations for an enhanced user experience.',
+    'This Notes App is a modern, responsive web application designed to help users efficiently manage their notes with a priority-based organization system., the app features a clean, intuitive interface that supports creating, editing, and deleting notes, each categorized into three priority levels — Urgent, High, and Low. Users can register, log in, and manage their notes in real time, with changes reflected instantly without page reloads. The App offers additional features like grid/list view switching, responsive layouts for mobile and desktop, and smooth animations for an enhanced user experience.',
 }
 
 export default function RootLayout({ children }) {
